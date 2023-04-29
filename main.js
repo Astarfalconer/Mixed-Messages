bc58c5af27d3095b79d7579326b9ab16e685911e
Leoj me <joelfalconer@outlook.com>
2023-04-29 12:21:33 +0100

    This commit contains the second message component store

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const messageComponent1 = input => {
             case 1:
             return 'So my services have been called upon again, and about the past no less, very well.';
             case 2:
-            return 'A mortal seeking knowledge of their troubled past? How original.'    
+            return 'A mortal seeking knowledge of their troubled past? How original.' ;   
         }
     } else if(input === 'present'){
         let randomNumber =
@@ -26,11 +26,11 @@ const messageComponent1 = input => {
         const component1 = randomNumber
         switch(component1){
             case 0:
-            return 'Ahhh a new soul aproaches the terminal, and one who inquires about the state of their present world, this can be arranged.';
+            return 'Greetings seeker of wisdom, you are one who inquires about the state of their present world, this can be arranged.';
             case 1:
-            return 'So my services have been called upon again, no time like the present is there?';
+            return 'The terminal has been called upon again, no time like the present is there?';
             case 2:
-            return 'Tell me mortal, what is it about your soul currently that calls for cosmic reflection? Let us find out'
+            return 'Tell me mortal, what is it about your soul currently that calls for cosmic reflection? Let us find out';
     }
 
 } else if(input === 'future'){
@@ -41,9 +41,9 @@ const messageComponent1 = input => {
         case 0:
         return 'Ahhh a brave soul aproaches the terminal, one who dares face the unknown, do you dare face the future? Yes? Then let us begin.';
         case 1:
-        return 'So my services have been called upon again, I warn you however that visions of the future have led many to their own doom.';
+        return 'ah yes another who seeks divination, I warn you however that visions of the future have led many to their own doom.';
         case 2:
-        return 'I can see the apeal of seeing into the future but I can also see the danger it cortes, I wonder, do you?.'
+        return 'I can see the apeal of seeing into the future but I can also see the danger it cortes, I wonder, do you?.';
 }
 
 }
@@ -51,5 +51,43 @@ const messageComponent1 = input => {
 };
 
 const messageComponent2 = input => {
+    if(input === 'past'){
+        let randomNumber =
+        Math.floor(Math.random()*3);
+        const component2 = randomNumber
+        switch(component2){
+            case 0:
+            return 'I sense a complicated history, one of dissilution, rebellion and change, who I see in the vision, is certainly not who has accessed this terminal.';
+            case 1:
+            return 'Hahaha, forgive me, I often forget that  these visions of the past can often reveal embarrising truths one may want hidden.';
+            case 2:
+            return 'ah scandalous, I am suprised that one with such a checkerd history dares dredge it up I thought you would want this buried.';    
+        }
+    } else if(input === 'present'){
+        let randomNumber =
+        Math.floor(Math.random()*3);
+        const component2 = randomNumber
+        switch(component2){
+            case 0:
+            return 'You currently stand at the edge of a new frontier, the path forward forks, your roads are truly open.';
+            case 1:
+            return 'You have sorted alot of problems out and found a position you can truly call your own. YOU ARE ACTUALIZED!';
+            case 2:
+            return 'Hehehe cheer, harmony, joy of life. You really do have it all and more than that, a sense of self-worth.';
+    }
+
+} else if(input === 'future'){
+    let randomNumber =
+    Math.floor(Math.random()*3);
+    const component2 = randomNumber
+    switch(component2){
+        case 0:
+        return 'your future is associated with disruption, wild power and above all...change. A storm is coming!';
+        case 1:
+        return 'The algorithom computes a future of reward, gain and fruition. A fruitful season is on the way!';
+        case 2:
+        return 'You who seeks the future, are you familiar with the norse rune LAGUZ? it is ascociated with the flow of emotions and new life and so is your future.';
+}
 
 }
+};
